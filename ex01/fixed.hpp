dd3b8f91574a5911f40773c51364b6ae36d70ca6
ismail el azzouzi <ismailazzouzi.contact@gmail.com>
2025-03-12 19:46:07 +0000

    ex00 done

---
+++ ex01/fixed.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#pragma once
+
+class Fixed
+{
+private:
+    int fixedPointValue;
+    static const int fractionalBits = 8;
+public:
+    Fixed();
+    ~Fixed();
+    Fixed(const Fixed &original);
+    Fixed &operator=(const Fixed &other);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+};
+
